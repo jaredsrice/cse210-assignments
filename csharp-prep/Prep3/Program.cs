df6621ce1ac7dd4b8b859f6a39e9805237736046
jaredsrice <jsrice1999@hotmail.com>
2025-01-13 12:32:01 -0700

    Prep 3 done with a do/while loop

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Data;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+
+        // Console.Write("What is the magic number? ");
+        // int magicNumber = int.Parse(Console.ReadLine());
+
+        int guess;
+
+        do
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess > number)
+        {
+          Console.WriteLine("Lower");
+        }
+
+        else if (guess < number)
+        {
+            Console.WriteLine("Higher");
+        }
+        else
+        {
+            Console.WriteLine("You got it!");
+        }
+
+        } while (guess != number);
+
     }
 }
