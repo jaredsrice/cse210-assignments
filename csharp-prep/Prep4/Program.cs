b1ff9b7f99bda37d50384f48a8c3394c6b828651
jaredsrice <jsrice1999@hotmail.com>
2025-01-13 13:38:17 -0700

    Prep4 code completed, no stretch challenges done here

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,55 @@
-using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int number;
+
+            Console.WriteLine("\nEnter a list of numbers, type 0 when finished.");
+
+            do
+            {
+                Console.Write("\nEnter a Number: ");
+                number = int.Parse(Console.ReadLine());
+
+                if (number != 0)
+                {
+                    numbers.Add(number);
+                }
+            }
+            while (number != 0);
+
+        //  Computing the Sum
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+        
+        Console.WriteLine($"\nThe sum is: {sum} ");
+
+        // Computing the Average
+        float average = (float)sum / numbers.Count;
+        Console.WriteLine($"\nThe average is: {average} ");
+
+        // Finding the largest number
+        // int maxNumber = numbers.Max();
+
+        int maxNumber = int.MinValue;
+
+        foreach (int num in numbers)
+        {
+            if (num > maxNumber)
+            {
+                maxNumber = num;
+            }
+        }
+
+        Console.WriteLine($"\nThe largest number is: {maxNumber}\n");
+
+        
     }
 }
