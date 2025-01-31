07d379263733c52eb4f2a2e29ab5d2ead5250f96
jaredsrice <jsrice1999@hotmail.com>
2025-01-31 14:49:32 -0700

    Program finished

--- prove/Develop02/Prompt Generator.cs
+++ prove/Develop02/Prompt Generator.cs
@@ -1,7 +1,27 @@
 using System;
-using System.Collections.Generic;
 
 public class PromptGenerator
 {
-    
+    private List<string> _prompts;
+    private Random _random;
+
+    public PromptGenerator()
+    {
+        _prompts = new List<string>
+        {
+            "What was the best part of your day?",
+            "What are you grateful for today?",
+            "What is one thing you would like to do better tomorrow?",
+            "What is something you learned today?",
+            "What made you smile today?"
+        };
+
+        _random = new Random();
+    }
+
+    public string GetPrompt()
+    {
+        int index = _random.Next(_prompts.Count);
+        return _prompts[index];
+    }
 }
