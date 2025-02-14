cf8e9b0b8d95809d6357d60af56d04217d96d66f
jaredsrice <jsrice1999@hotmail.com>
2025-02-13 17:02:26 -0700

    Reference class done, scripture class started

--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -1,6 +1,38 @@
 using System;
+using System.Collections.Concurrent;
 
 class Reference
 {
-    
+    private string _book;
+    private int _chapter;
+    private int _startingVerse;
+    private int _endingVerse;
+
+    public Reference(string book, int chapter, int verse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _startingVerse = verse;
+        _endingVerse = verse;
+    }
+
+    public Reference(string book, int chapter, int startVerse, int endVerse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _startingVerse = startVerse;
+        _endingVerse = endVerse;
+    }
+
+    public string GetDisplayText()
+    {
+        if (_startingVerse == _endingVerse)
+        {
+            return $"{_book} {_chapter} : {_startingVerse}";
+        }
+        else
+        {
+            return $"{_book} {_chapter} : {_startingVerse} - {_endingVerse}";
+        }
+    }
 }
--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -2,5 +2,12 @@ using System;
 
 class Scripture
 {
-    
+    private Reference _reference;
+    private List<Word> _referenceWords;
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _referenceWords = new List<Word>();
+    }
 }
