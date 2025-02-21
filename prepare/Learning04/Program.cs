e817f3e75c370b930c8cdbea8359d22e8f78485b
jaredsrice <jsrice1999@hotmail.com>
2025-02-20 22:22:41 -0700

    Learning Activity Done

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment = new Assignment("Shrek", "Ogre Studies");
+        Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment mathAssignment = new MathAssignment("Donkey", "Quantum Physics", "1.6", "8-21");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WritingAssignment writingAssignment = new WritingAssignment("Fiona", "The Art of Ogre Diplomacy", "When an Ogre Becomes an Onion");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
