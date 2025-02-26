8620833479335dd9dc86c9f868b9eb1d9f3f7a48
jaredsrice <jsrice1999@hotmail.com>
2025-02-25 18:25:13 -0700

    Program and Menu classes done

---
+++ prove/Develop04/Menu.cs
@@ -0,0 +1,73 @@
+using System;
+
+public class Menu
+{
+    public void Start()
+    {
+        while (true)
+        {
+            DisplayMenu();
+            int choice = GetUserChoice();
+            if (choice == 4)
+            {
+                Console.WriteLine("\nProgram go bye bye.\n");
+                return;
+            }
+            RunActivity(choice);
+            Console.WriteLine();
+        }
+    }
+    public void DisplayMenu()
+    {
+        Console.WriteLine("\nPlease choose an activity:");
+        Console.WriteLine("1. Breathing Activity");
+        Console.WriteLine("2. Reflecting Activitiy");
+        Console.WriteLine("3. Listing Activity");
+        Console.WriteLine("4. Quit\n");
+    }
+    
+    public int GetUserChoice()
+    {
+        Console.Write("Please enter your choice: ");
+        string input = Console.ReadLine();
+        if (int.TryParse(input, out int choice))
+        {
+            return choice;
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a number.");
+            return -1;
+        }
+    }
+
+    public void RunActivity(int choice)
+    {
+        if (choice == 1)
+        {
+            Console.WriteLine("\nStarting Breathing Activity.");
+            BreathingActivity activityOne = new BreathingActivity();
+            // activityOne.RunActivity();
+        }
+
+        else if (choice == 2)
+        {
+            Console.WriteLine("\nStarting Reflecting Activity.");
+            ReflectingActivity activityTwo = new ReflectingActivity();
+            // activityTwo.RunActivity();
+        }
+
+        else if (choice == 3)
+        {
+            Console.WriteLine("\nStarting Listing Activity.");
+            ListingActivity activityThree = new ListingActivity();
+            // activityThree.RunActivity();
+        }
+
+        else
+        {
+            Console.WriteLine("Wrong choice, womp womp. Try again.");
+        }
+    }
+
+}
