21a4ae5b6efa276346ec8228cd7b0ce5e2047ff4
jaredsrice <jsrice1999@hotmail.com>
2025-02-28 20:29:22 -0700

    comments for submission

--- prove/Develop04/BreathingActivity.cs
+++ prove/Develop04/BreathingActivity.cs
@@ -1,6 +1,12 @@
 using System;
 
 public class BreathingActivity: Activity
+
+// Here I would also consider hitting my creativity boundary, I put a lot of effort into
+// making sure that the breathing activity worked with as many numbers as possible, and
+// wasn't obnoxiously long for a single cycle. For example, 6 seconds is one cycle of 2 seconds
+// each, whereas 31 seconds is 2 cycles of 5 seconds each, with the last exhaling cycle
+// being 6 seconds. Bunch of math and error prevention in here. 
 {
     public BreathingActivity()
     : base ("Breathing Activity", "Practice relaxation and deep, controlled breathing by following this guided mediation.")
--- prove/Develop04/Countdowns.cs
+++ prove/Develop04/Countdowns.cs
@@ -2,6 +2,8 @@ using System;
 
 public class Countdowns
 {
+
+    // I created two countdowns, one is a spinner and the other is a bar countdown, which I consider as my creativity.
    public static void SpinnerPause(int seconds)
    {
         DateTime startTime = DateTime.Now;
