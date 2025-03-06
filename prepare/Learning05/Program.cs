300cd8ba9239a3686fb141a25d189bbeee1937a1
jaredsrice <jsrice1999@hotmail.com>
2025-03-06 16:10:10 -0700

    prepare 5 done

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square1 = new Square("Blue", 2);
+        shapes.Add(square1);
+
+        Rectangle rectangle1 = new Rectangle("Red", 2, 3);
+        shapes.Add(rectangle1);
+
+        Circle circle1 = new Circle("Green", 2);
+        shapes.Add(circle1);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();  
+            double area = shape.GetArea();    
+
+            area = Math.Round(area,2);
+
+            Console.WriteLine($"Color: {color}, Area: {area}");
+        }
     }
 }
