9b7edd1d6b09c0579cb6bf4100d3cab3745becde
jaredsrice <jsrice1999@hotmail.com>
2025-03-09 20:16:33 -0600

    Moved comments to main

--- prove/Develop04/Countdowns.cs
+++ prove/Develop04/Countdowns.cs
@@ -1,10 +1,8 @@
 using System;
 
 public class Countdowns
-{
-
-    // I created two countdowns, one is a spinner and the other is a bar countdown, which I consider as my creativity.
-   public static void SpinnerPause(int seconds)
+{   
+    public static void SpinnerPause(int seconds)
    {
         DateTime startTime = DateTime.Now;
         DateTime endTime = startTime.AddSeconds(seconds);
--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -7,4 +7,16 @@ public class Program
         Menu menu = new Menu();
         menu.Start(); 
     }
+
+        // I created two countdowns & the countdown class, one is a spinner 
+        // and the other is a bar countdown, which I consider as my creativity.
+
+        // In my BreathingActivity class I would also consider hitting my creativity boundary, I put 
+        // a lot of effort into making sure that the breathing activity worked with as many numbers 
+        // as possible, and wasn't obnoxiously long for a single cycle. 
+        // For example, 6 seconds is one cycle of 2 seconds each, 
+        // whereas 31 seconds is 2 cycles of 5 seconds each, with the last exhaling cycle
+        // being 6 seconds. Bunch of math and error prevention in here. 
+
+
 }
